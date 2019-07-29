b375166342180f6d03f67c79451d1660676ea6a4
hyeriyang <goumtingi@naver.com>
2019-07-30 00:32:49 +0900

    4th commit

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'uq1#muojtn5k#)f*zbkgewc9!ma4d*
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
